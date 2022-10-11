4e822feb1613ef1b6f931f91a368b91fd3547204
mhwdvs <matt@mhwdvs.com>
2022-10-12 00:04:25 +0800

    Fix native x64 build

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,24 @@
-//use embedded_graphics::pixelcolor::Rgb888;
+use embedded_graphics::{
+    draw_target::DrawTarget, mono_font::ascii::*, mono_font::*, pixelcolor::Rgb888,
+    prelude::Dimensions, text::Alignment, text::Text, Drawable,
+};
 
 mod matrix;
-//#[cfg(not(all(target_arch = "arm", target_os = "linux", target_env = "gnu")))]
-// mod mock_matrix;
-//#[cfg(all(target_arch = "arm"))]
-mod rpi_matrix;
+use matrix::Matrix;
 
 pub fn main() {
-    #[cfg(not(all(target_arch = "arm", target_os = "linux", target_env = "gnu")))]
-    let mut framebuffer = matrix::get_matrix();
+    let matrix = Matrix::new();
 
-    #[cfg(all(target_arch = "arm", target_os = "linux", target_env = "gnu"))]
-    let matrix = matrix::get_matrix();
-    // establish frame buffer
-    #[cfg(all(target_arch = "arm", target_os = "linux", target_env = "gnu"))]
-    let mut framebuffer = matrix.offscreen_canvas();
-
-    //let character_style = MonoTextStyle::new(&FONT_6X10, Rgb888::new(0xff, 0xff, 0xff));
+    let character_style = MonoTextStyle::new(&FONT_6X10, Rgb888::new(0xff, 0xff, 0xff));
 
     loop {
-        //_ = Text::with_alignment(
-        //    "10:52PM",
-        //    display.bounding_box().center(),
-        //    character_style,
-        //    Alignment::Center,
-        //)
-        //.draw(&mut display);
+        let mut canvas = matrix.get_canvas();
+        let p_center = canvas.bounding_box().center();
+
+        _ = Text::with_alignment("10:52PM", p_center, character_style, Alignment::Center)
+            .draw(&mut canvas);
 
         // swap frame buffer
-        //#[cfg(all(target_arch = "arm", target_os = "linux", target_env = "gnu"))]
-        //framebuffer = matrix.swap(canvas);
+        matrix.swap_framebuffer();
     }
 }
--- src/matrix.rs
+++ src/matrix.rs
@@ -1,17 +1,60 @@
-//#[cfg(not(all(target_arch = "arm", target_os = "linux", target_env = "gnu")))]
-//use {
-//    crate::mock_matrix,
-//    embedded_graphics::{mock_display::MockDisplay, pixelcolor::Rgb888},
-//};
-//#[cfg(all(target_arch = "arm"))]
-use {crate::rpi_matrix, rpi_led_matrix::LedMatrix};
+#[cfg(not(all(target_arch = "arm", target_os = "linux", target_env = "gnu")))]
+use embedded_graphics::{
+    mock_display::MockDisplay, pixelcolor::Rgb888, prelude::*, primitives::Rectangle,
+};
 
-//#[cfg(all(target_arch = "arm", target_os = "linux", target_env = "gnu"))]
-pub fn get_matrix() -> LedMatrix {
-    return rpi_matrix::get_matrix();
+#[cfg(all(target_arch = "arm"))]
+use {
+    crate::rpi_matrix,
+    rpi_led_matrix::{LedCanvas, LedMatrix},
+};
+
+pub struct Matrix {
+    #[cfg(all(target_arch = "arm"))]
+    rpi_led_matrix: LedMatrix,
+    #[cfg(all(target_arch = "arm"))]
+    rpi_led_canvas: LedCanvas,
 }
 
-//#[cfg(not(all(target_arch = "arm", target_os = "linux", target_env = "gnu")))]
-//pub fn get_matrix() -> MockDisplay<Rgb888> {
-//    return mock_matrix::get_matrix();
-//}
+impl Matrix {
+    #[cfg(all(target_arch = "arm", target_os = "linux", target_env = "gnu"))]
+    pub fn new() -> Matrix {
+        let mut options = LedMatrixOptions::new();
+        _ = options.set_brightness(100);
+        options.set_cols(64);
+        options.set_rows(32);
+        options.set_hardware_mapping("adafruit-hat-pwm");
+        options.set_limit_refresh(30);
+
+        let mut matrix = LedMatrix::new(Some(options), None).unwrap();
+
+        return Matrix {
+            rpi_led_matrix: matrix,
+            rpi_led_canvas: matrix.offscreen_canvas(),
+        };
+    }
+
+    #[cfg(not(all(target_arch = "arm", target_os = "linux", target_env = "gnu")))]
+    pub fn new() -> Matrix {
+        return Matrix {};
+    }
+
+    #[cfg(all(target_arch = "arm", target_os = "linux", target_env = "gnu"))]
+    pub fn get_canvas(&self) -> LedCanvas {
+        return rpi_led_matrix.offscreen_canvas();
+    }
+
+    #[cfg(not(all(target_arch = "arm", target_os = "linux", target_env = "gnu")))]
+    pub fn get_canvas(&self) -> MockDisplay<Rgb888> {
+        let rect = Rectangle::new(Point::new(0, 0), Size::new(64, 32));
+        return MockDisplay::from_points(rect.points(), Rgb888::new(0, 0, 0));
+    }
+
+    #[cfg(all(target_arch = "arm", target_os = "linux", target_env = "gnu"))]
+    pub fn swap_framebuffer(&self) {
+        rpi_led_canvas.swap();
+    }
+
+    #[cfg(not(all(target_arch = "arm", target_os = "linux", target_env = "gnu")))]
+    pub fn swap_framebuffer(&self) {}
+}
