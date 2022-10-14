486521893701c280f83c357f78cd87b595ecac36
mhwdvs <matt@mhwdvs.com>
2022-10-15 01:55:50 +0800

    Add buffer matrix object

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,9 @@ mod matrix;
 mod state;
 mod states;
 
+use std::sync::Arc;
+use std::sync::Mutex;
+
 use inputs::bh1750::BH1750;
 use matrix::Matrix;
 use state::State;
@@ -10,7 +13,8 @@ use states::region_select::region_select_state;
 use states::time::time_state;
 
 pub fn main() {
-    let mut matrix = Matrix::new(None);
+    let matrix = Matrix::new(None);
+    let buffer_matrix = Matrix::new(None);
 
     // initial state = RegionSelect
     let mut current_state = State::RegionSelect;
@@ -23,10 +27,11 @@ pub fn main() {
 
     let mut brightness_update = 0;
     loop {
-        if (brightness_update == 10) {
+        if brightness_update == 10 {
             let brightness = BH1750::get_brightness();
             println!("{}", brightness);
-            matrix = matrix.set_brightness(brightness);
+
+            matrix = buffer_matrix.set_brightness(brightness);
             brightness_update = 0;
         } else {
             brightness_update += 1;
