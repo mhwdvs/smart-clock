8d28f4bf18f7b66db1c8631a1240e05007641b33
mhwdvs <matt@mhwdvs.com>
2022-10-14 22:05:02 +0800

    Make buffer mutable

--- src/inputs/bh1750.rs
+++ src/inputs/bh1750.rs
@@ -17,21 +17,21 @@ pub struct BH1750 {}
 
 impl BH1750 {
     pub fn test_light_conn() -> Result<(), InputError> {
+        I2c.with_bus(1);
         let mut channel = I2c::new()?;
         channel.set_slave_address(BH1750_ADDR);
-        channel.with_bus(1);
         //channel.smbus_read_byte();
 
         Ok(())
     }
 
     pub fn get_light() -> Result<(), InputError> {
+        I2c.with_bus(1);
         let mut channel = I2c::new()?;
         channel.set_slave_address(BH1750_ADDR);
-        channel.with_bus(1);
 
-        let buf: [u8; 2];
-        let result = channel.read(buf).unwrap();
+        let mut buf: [u8; 2];
+        let result = channel.read(&mut buf).unwrap();
         if result != 2 {
             return Err(InputError::LightReadErr);
         }
